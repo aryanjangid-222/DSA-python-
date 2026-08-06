6e0604d3e1eae5e97cf3dcb8031aeac48289fee1
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-08-06 15:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (70.48%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        out = []
+        for el in nums:
+            out.append(el)
+        for el in nums:
+            out.append(el)
+        return out
+        
