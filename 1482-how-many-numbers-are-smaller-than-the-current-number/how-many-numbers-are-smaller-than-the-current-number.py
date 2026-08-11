ace89489f40cbbeab1a3b204e29d5fd6f481c59c
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-08-11 23:21:32 +0530

    Time: 1 ms (89.88%) | Memory: 12.4 MB (44.32%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def smallerNumbersThanCurrent(self, nums):
-        out = []
-        for el in nums:
-            a = 0
-            for el1 in nums:
-                if el1<el:
-                    a += 1
-            out.append(a)
-        return out
+        sorted_nums = sorted(nums)
+        
+        lookup = {}
+        for idx, num in enumerate(sorted_nums):
+            if num not in lookup:
+                lookup[num] = idx
+        return [lookup[num] for num in nums]
+               
+        
