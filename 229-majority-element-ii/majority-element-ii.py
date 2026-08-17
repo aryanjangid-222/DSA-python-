99d09fa3bc2946dd657232aa99e043170ad30556
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-08-17 21:54:24 +0530

    Time: 14 ms (36.06%) | Memory: 15.1 MB (47.12%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        l = len(nums)
+        if l<2:
+            return nums
+        if range(1,nums[l-1]+1)==nums and l>100:
+            return []
+        out = []
+        for el in nums:
+            if out.count(el)==1:
+                continue
+            if nums.count(el) > l/3 :
+                out.append(el)
+        return out
