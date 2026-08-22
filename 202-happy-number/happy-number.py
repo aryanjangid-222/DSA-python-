cb2fb77457a46fbe4b7ae9a2f68b475c2e93afdc
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-08-22 18:28:09 +0530

    Time: 8 ms (8.12%) | Memory: 12.3 MB (89.44%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isHappy(self, n):
+        c = 0
+        while True:
+            c += 1
+            if c==20:
+                return False
+            if n==1:
+                return True
+            a = str(n)
+            n = 0
+            for el in a:
+                n += int(el)**2
+            if n == 1:
+                return True
