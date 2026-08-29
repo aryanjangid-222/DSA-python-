49c599a6811b809f030a3b06eb05041fcbbe5871
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-08-29 20:25:44 +0530

    Time: 21 ms (98.24%) | Memory: 19.2 MB (98.90%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        out = []
+        pro = 1
+        i = -1
+        a = 0
+        for el in nums:
+            i += 1
+            if el != 0:
+                pro *= el
+            else:
+                a += 1
+        if a == 1:
+            for el in nums:
+                if el != 0:
+                    out.append(0)
+                else:
+                    out.append(pro)
+        elif a > 1:
+            for el in nums:
+                out.append(0)
+        else:
+            for el in nums:
+                out.append(pro/el)
+        return out
