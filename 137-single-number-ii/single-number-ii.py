3ad69f0d15db1076b505a2d6828a15cb8448298d
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-01 17:42:03 +0530

    Time: 825 ms (6.92%) | Memory: 13.4 MB (70.65%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -2,5 +2,5 @@ class Solution(object):
     def singleNumber(self, nums):
         for el in nums:
             if nums.count(el) == 1:
-                return el
+                return el 
             
