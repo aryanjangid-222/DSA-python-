39902b4c3267b01efd72f31f96e20646fa357718
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-02 17:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (15.74%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.py
+++ 4354-unique-middle-element/unique-middle-element.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def isMiddleElementUnique(self, nums):
-        m = len(nums)//2
-        c = 0
-        mid = nums[m]
-        for el in nums:
-            if el == mid:
-                c += 1
-        return c == 1
+        mid = nums[len(nums)//2]
+        return nums.count(mid) == 1
         
