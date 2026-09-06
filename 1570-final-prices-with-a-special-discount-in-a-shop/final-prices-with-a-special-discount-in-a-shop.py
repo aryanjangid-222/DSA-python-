ebcd0512d91191e1d368cf7d1c47e659e430300d
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-07 00:03:37 +0530

    Time: 14 ms (13.46%) | Memory: 12.4 MB (52.14%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -2,13 +2,15 @@ class Solution(object):
     def finalPrices(self, prices):
         out = []
         for i in range(len(prices)):
+            diss = True
             e = prices[i]
-            el = 0
             for j in range(i+1,len(prices)):
                 if prices[j] <= e:
-                    el = prices[j]
+                    out.append(e - prices[j])
+                    diss = False
                     break
-            out.append(e - el)
+            if diss:
+                out.append(e)
         
         return out
         
