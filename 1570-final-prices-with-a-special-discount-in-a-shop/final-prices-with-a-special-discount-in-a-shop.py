2bc1326c3388882b73da42a9e7e55feb5372fe33
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-06 23:57:08 +0530

    Time: 9 ms (35.74%) | Memory: 12.5 MB (13.37%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def finalPrices(self, prices):
+        out = []
+        for i in range(len(prices)):
+            e = prices[i]
+            el = 0
+            for j in range(i+1,len(prices)):
+                if prices[j] <= e:
+                    el = prices[j]
+                    break
+            out.append(e - el)
+        
+        return out
+        
