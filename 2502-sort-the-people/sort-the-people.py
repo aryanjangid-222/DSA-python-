3235a043affae0784c4118706308d0fc6a74d662
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-10 23:43:49 +0530

    Time: 1045 ms (5.14%) | Memory: 12.7 MB (52.95%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        n = len(heights)
+        for i in range(n):
+            swapped = False
+            for j in range(0, n - i - 1):
+                if heights[j] > heights[j + 1]:
+                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
+                    names[j], names[j + 1] = names[j + 1], names[j] 
+                    swapped = True
+
+            if not swapped:
+                break
+        
+        return names[::-1]
