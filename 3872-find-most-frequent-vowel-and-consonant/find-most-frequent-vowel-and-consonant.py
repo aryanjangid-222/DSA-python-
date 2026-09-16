befa5c1dedd21d9b8130efaa4fc15ff512628227
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-16 13:55:46 +0530

    Time: 7 ms (38.94%) | Memory: 12.4 MB (20.46%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,17 +1,15 @@
 class Solution(object):
     def maxFreqSum(self, s):
+        li = list(set(s))
+        check = "aeiou"
         max_freV = 0
         max_freC = 0
-        org = list(set(s))
-        vowal = "aeiou"
-        for el in org:
+        for el in li:
             c = s.count(el)
-            if el in vowal:
+            if el in check:
                 if c > max_freV:
                     max_freV = c
-                continue
-            elif c > max_freC:
+            elif max_freC < c:
                 max_freC = c
         
-        return max_freV + max_freC
-        
+        return max_freC + max_freV
