02fee3eaf2416ad7027c24da3fa0e664b75b5a93
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-17 23:08:08 +0530

    Time: 3 ms (88.58%) | Memory: 13.7 MB (12.20%) - LeetSync

---
+++ 979-di-string-match/di-string-match.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def diStringMatch(self, s):
+        d = len(s)
+        i = 0
+        out = []
+        c = ""
+        for el in s:
+            if el == "D":
+                out.append(d)
+                d -= 1
+                c = el
+            else:
+                out.append(i)
+                i += 1
+                c = el
+        if c == "I":
+            out.append(i)
+        else:
+            out.append(d)
+        return out
