8d569776e199ea76a61b814b3f10657348fce149
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-19 00:21:04 +0530

    Time: 33 ms (59.86%) | Memory: 12.4 MB (56.18%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -1,15 +1,8 @@
 class Solution(object):
     def sumOfMultiples(self, n):
-        check = range(1,n+1)
         s = 0
-        for el in check:
-            if el % 3 == 0:
+        for el in range(1,n+1):
+            if el%3==0 or el%5==0 or el%7==0:
                 s += el
-                continue
-            elif el % 5 == 0:
-                s += el
-                continue
-            elif el % 7 == 0:
-                s += el
-
+        
         return s
