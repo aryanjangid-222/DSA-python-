2b8022db52fbc339ca9ada4888ce1d11d4c150c1
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-19 22:56:58 +0530

    Time: 4 ms (37.04%) | Memory: 12.2 MB (96.30%) - LeetSync

---
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def checkPrimeFrequency(self, nums):
+        li = list(set(nums))
+        pr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        for el in li:
+            if nums.count(el) in pr:
+                return True
+        return False
