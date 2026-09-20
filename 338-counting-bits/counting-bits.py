9a1075bd866633173d7c7aebcc51517d1d22c68e
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-20 23:58:39 +0530

    Time: 723 ms (5.00%) | Memory: 17.8 MB (55.58%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def countBits(self, n):
+        out = []
+        for i in range(n+1):
+            n = i
+            c = -1
+            b = 0
+            while n != 0:
+                c += 1
+                if n%2:
+                    n -= 1
+                    b = (1*(10**c)) + b
+                else:
+                    n /= 2
+                    b = (0*(10**c)) + b
+            n = 0
+            while b != 0:
+                if b % 10 == 1:
+                    n += 1
+                b //= 10
+            out.append(n)
+        return out
