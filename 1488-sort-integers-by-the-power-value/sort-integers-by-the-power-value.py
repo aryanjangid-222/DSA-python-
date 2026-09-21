0727a446c77fa6d568198152a26476760305c0b9
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-21 23:20:38 +0530

    Time: 364 ms (63.42%) | Memory: 12.6 MB (31.71%) - LeetSync

---
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def getKth(self, lo, hi, k):
+        check = []
+        elem = []
+        for el in range(lo,hi+1):
+            p = 0
+            elem.append(el)
+            while el != 1:
+                if el % 2:
+                    p += 1
+                    el = el*3 + 1
+                else:
+                    p += 1
+                    el /= 2
+            check.append(p)
+        out = zip(*sorted(zip(check, elem)))
+        return out[1][k-1]
