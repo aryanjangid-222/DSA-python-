f9bd67e5fcf8bf274ead94d314981b2170e28c0c
aryanjangid-222 <aryanjangid0013@gmail.com>
2026-09-24 23:26:20 +0530

    Time: 7 ms (45.66%) | Memory: 12.3 MB (58.38%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.py
+++ 2159-two-out-of-three/two-out-of-three.py
@@ -6,14 +6,19 @@ class Solution(object):
                 continue
             if el in nums2:
                 res.append(el)
+                nums2.remove(el)
             elif el in nums3:
                 res.append(el)
+                nums3.remove(el)
         
         for el in nums2:
             if el in res:
                 continue
             if el in nums3:
                 res.append(el)
+                nums3.remove(el)
+
+        return res
         
         return res
         
